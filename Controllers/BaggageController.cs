6afb5ced128f30f9e440d1309b6099474e7bd935
Tatiana <tmelnik03@gmail.com>
2025-02-23 22:35:41 +0300

    Добавлены поиск свободных машин и запрос маршрута

--- Controllers/BaggageController.cs
+++ Controllers/BaggageController.cs
@@ -1,21 +1,34 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Http;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
 
 [ApiController]
 public class BaggageController : ControllerBase
 {
+    // ОГРАНИЧЕНИЕ НА КОЛИЧЕСТВО МАШИН
+
     private static readonly HttpClient client = new();
+
+    // Вместимость одной машины
+    private const double VehicleCapacity = 500.0;
+
+    // Словарь для хранения машин
+    private static readonly Dictionary<string, string> vehicleNodeMapping = new();
+
     public class BaggageLoadRequest
     {
         public string AircraftId { get; set; }
-        public float BaggageWeight { get; set; }
+        public double BaggageWeight { get; set; }
         public string AircraftCoordinates { get; set; }
     }
 
     public class BaggageUploadRequest
     {
         public string AircraftId { get; set; }
-        public float BaggageWeight { get; set; }
+        public double BaggageWeight { get; set; }
         public string AircraftCoordinates { get; set; }
     }
 
@@ -69,6 +82,42 @@ public class BaggageController : ControllerBase
         return null; // Если запрос не удался, возвращаем null
     }
 
+    private async Task<string[]?> GetRouteAsync(string from, string to)
+    {
+        client.DefaultRequestHeaders.Accept.Clear();
+        client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        // Создаем тело запроса
+        var jsonData = new
+        {
+            from,
+            to,
+            type = "baggage"
+        };
+        var json = JsonConvert.SerializeObject(jsonData);
+        var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+        // Отправляем POST-запрос
+        var response = await client.PostAsync("/route", content);
+
+        if (response.IsSuccessStatusCode)
+        {
+            // Чтение и десериализация ответа
+            var result = await response.Content.ReadAsStringAsync();
+            return JsonConvert.DeserializeObject<string[]>(result);
+        }
+        else if (response.StatusCode == System.Net.HttpStatusCode.NotFound)
+        {
+            Console.WriteLine("Маршрут не найден.");
+        }
+        else
+        {
+            // Обработка других ошибок
+            var errorContent = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"Произошла ошибка: {response.ReasonPhrase}, содержимое: {errorContent}");
+        }
+
+        return null; // Если запрос не удался, возвращаем null
+    }
     [HttpPost("load")]
     public async Task<ActionResult<BaggageResponse>> LoadBaggage([FromBody] BaggageLoadRequest request)
     {
@@ -78,34 +127,85 @@ public class BaggageController : ControllerBase
             return BadRequest(new ErrorResponse { Error = "Неверный запрос" });
         }
 
-        // Регистрация новой машины
-        string vehicleType = "baggage"; 
-        var response = await RegisterVehicleAsync(vehicleType);
+        // Считаем количество необходимых для перевозки машин
+        int countCars = (int)Math.Ceiling((double)request.BaggageWeight / VehicleCapacity);
 
-        if (response != null)
+        for (int i = 0; i < countCars; i++)
         {
-            Console.WriteLine($"Node ID: {response.NodeId}, Vehicle ID: {response.VehicleId}");
-            // Здесь логика
+            // Ищем свободную машину
+            var foundVehicle = vehicleNodeMapping.FirstOrDefault(nodeId => nodeId.Value != "в пути");
+
+            // Проверяем, что такая машина найдена
+            if (foundVehicle.Key != null)
+            {
+                string availableVehicleId = foundVehicle.Key;
+                string availableVehiclePlace = foundVehicle.Value;
+
+                Console.WriteLine($"Найдена доступная машина: ID = {availableVehicleId}, местоположение = {availableVehiclePlace}");
+
+                //Дефолт для проверки
+                string[] routePoints = ["node1", "node2", "node3"];
+
+                // Получаем маршрут
+                // Реальный запрос
+                //var routePoints = await GetRouteAsync(availableVehiclePlace, request.AircraftCoordinates);
+                if (routePoints != null)
+                {
+                    // Обработка полученных данных
+                    foreach (var point in routePoints)
+                    {
+                        Console.WriteLine("Маршрут:");
+                        Console.WriteLine($"ID точки: {point}");
+                        // В цикле:
+                        // Запрашиваем разрешение на передвижение (повторно, если необходимо тоже)
+                        // Считаем время передвижения
+                        // Уведомляем о прибытии
+
+                        //Сообщаем, что прибыли на финальную точку
+                        //Возвращение в гараж
+                    }
+
+                }
+                else
+                {
+                    Console.WriteLine("Не удалось получить маршрут.");
+                }
+            }
+            else
+            {
+                // Регистрация новой машины
+                string vehicleType = "baggage";
+                var response = await RegisterVehicleAsync(vehicleType);
+
+                if (response != null)
+                {
+                    Console.WriteLine($"Node ID: {response.NodeId}, Vehicle ID: {response.VehicleId}");
+                    // Здесь логика
+                }
+                else
+                {
+                    Console.WriteLine("Запрос завершился неуспешно, данные отсутствуют.");
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
+            // Здесь должна быть логика обработки загрузки
+
+
+            // Если свободных машин нет
+            bool waiting = false; // Заменить на логику
+            return Ok(new BaggageResponse { Waiting = waiting });
         }
-        else
-        {
-            Console.WriteLine("Запрос завершился неуспешно, данные отсутствуют.");
-        }
-
-
-
-
-
-
-
-
-
-        // Здесь должна быть логика обработки загрузки
-
-
-        // Если свободных машин нет
-        bool waiting = false; // Заменить на логику
-        return Ok(new BaggageResponse { Waiting = waiting });
+        return Ok(new BaggageResponse { Waiting = true });
     }
 
     [HttpPost("upload")]
